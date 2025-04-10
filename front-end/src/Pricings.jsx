662334adc502358565e99bceec1befb10047371a
shabaz khan <135216873+Shabazkhan1352@users.noreply.github.com>
2025-04-11 01:08:34 +0530

    pricing card done

--- front-end/src/Pricings.jsx
+++ front-end/src/Pricings.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import PricingCard from './components/PricingCard'
+import NavScrollExample from './components/NavScrollExample'
 
 const Pricings = () => {
+
+  const basic = ["Unlock Upto 10 Property Contacts", "Unlimited Search Accsess " ,"Basic support via email","No Live Property Support", "Ideal for users just starting their property search"]
+  const bussiness = ["Unlock Upto 25 Property Contacts", "Unlimited Search Accsess ","Get priority customer support","Access to exclusive city-based insights", "Great for agents, investors, or serious buyers" ]
   return (
-    <div>
-        <PricingCard />
+    <div className='bg-[#FAFAFA]'>
+
+      <NavScrollExample/>
+      <div>
+
+        <h1 className='text-center poppins-bold text-[36px] my-10'>Choose our Best Plans Here</h1>
+
+        <div className='flex justify-center items-center gap-15'>
+        <PricingCard plan="Basic" price="799" features={basic} mybgcolor= "white" mycolor="black" />
+        <PricingCard  plan="Bussiness" price="1199"  features={bussiness} />
+        </div>
+     
+      </div>
       
     </div>
   )
--- front-end/src/components/PricingCard.jsx
+++ front-end/src/components/PricingCard.jsx
@@ -1,50 +1,47 @@
 import React from 'react';
+import { MdOutlineDoneOutline } from "react-icons/md";
 
-const PricingCard = () => {
+
+const PricingCard = ({ plan, price, features,mybgcolor,mycolor }) => {
+
+ 
   return (
-    <div className="bg-black text-white rounded-xl w-[300px] shadow-lg overflow-hidden m-[50px]  ">
+    
+    <div style={{backgroundColor : mybgcolor || "black", color:mycolor || "white"}} className={` text-white rounded-xl w-[350px] shadow-lg overflow-hidden cursor-pointer transition-all transform hover:-translate-y-5  `}>
       <div className="p-6 text-center">
-        <h2 className="text-xl poppins-semibold text-[22px] mb-2">Basic Plan</h2>
-        <p className="text-sm text-gray-400 poppins-regular text-[14px] mb-6">
+        <h2 style={{color:mycolor|| "#D4D4D4"}} className="text-2xl text-[#D4D4D4] capitalize    poppins-semibold  mb-2"><span className='text-[#9393ff]'>{plan}</span> Plan</h2>
+        <p style={{color:mycolor|| "text-gray-400"}} className="text-sm text-gray-400 poppins-regular text-[14px] mb-6">
           Best for brands that need designs and websites.
         </p>
 
         <div className="text-3xl font-bold mb-1 poppins-semibold text-[22px]">
-          1099 <span className="text-sm poppins-regular text-[18px]"> / month</span>
+          {price} <span style={{color:mycolor|| "text-gray-400"}} className="text-sm poppins-regular text-[16px] text-[#D4D4D4]">/month</span>
         </div>
-        <p className="text-sm text-gray-400 poppins-regular text-[14px] mb-6">No hidden charges. Cancel anytime.
+        <p style={{color:mycolor|| "text-gray-400"}} className="text-sm text-gray-400 poppins-regular text-[14px] mb-6">No hidden charges. Cancel anytime.
 
-</p>
+        </p>
 
         <ul className="space-y-3 text-sm poppins-regular text-[14px]">
-          <li className="flex items-center ">
-            <span className="text-green-400 mr-2">✔</span>
-            Browse property listings
 
-          </li>
-          <li className="flex items-center">
-            <span className="text-green-400 mr-2">✔</span>
-            Contact property owners directly
+          {features.map((item, i) => {
 
-          </li>
-          <li className="flex items-center">
-            <span className="text-green-400 mr-2 ">✔</span>
-            Schedule property visits
-          </li>
-          <li className="flex items-center">
-            <span className="text-green-400 mr-2">✔</span>
-            Basic support via email
+            return (
+             
+                <li key={i} className="text-start flex items-center gap-2 ">
+                 <span><MdOutlineDoneOutline style={{color:mycolor|| "text-gray-400"}} className='text-[18px ] text-white' /></span>
+                  {item}
+
+                </li>
+             
+            )
+          })}
 
 
-          </li>
-          <li className="flex items-center">
-            <span className="text-green-400 mr-2">✔</span>
-            Live priority support
-          </li>
+          
         </ul>
       </div>
 
-      <div className="bg-lime-400 text-center py-3 cursor-pointer hover:bg-lime-500 transition-all">
+      <div className="bg-[#9393ff] text-center py-3 cursor-pointer hover:bg-[#9393ffa5] transition-all">
         <button className="text-black font-semibold cursor-pointer poppins-semibold text-[22px]  ">Subscribe Now</button>
       </div>
     </div>
